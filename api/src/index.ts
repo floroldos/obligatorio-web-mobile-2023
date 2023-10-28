11220b74064009e3186d4e99dd3bc3c763ded285
Gonzalo <87908978+Gonza50@users.noreply.github.com>
2023-10-28 17:10:52 -0300

    Users esta en mongo

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,26 +2,32 @@ import express from 'express'
 import juegosRouter from './routes/juegos'
 import temasRouter from './routes/temas'
 import actividadesRouter from './routes/actividades'
-import userRoutere from './routes/users'
-var jwt = require('jsonwebtoken');
+import userRouter from './routes/users'
+import mongoose from 'mongoose'
+//const jwt = require('jsonwebtoken');
+
 
 const app = express()
-app.use(express.json()) //middleware
 
-const PORT = 3000
+// Middleware //
+app.use(express.json())
+app.use('/api', juegosRouter, temasRouter, userRouter, actividadesRouter);
 
-app.get('/test', (req, res) => {    
-    console.log("hello world");
-    res.send('V 1.1')
-})
+const PORT = process.env.PORT || 3000;
 
-app.use('/api/jueos', juegosRouter)
-app.use('/api/temas', temasRouter)
-app.use('/api/actividades', actividadesRouter)
-app.use('/api/Users', userRoutere)
+app.get('/', (req, res) => {
+    console.log("Api corriendo")
+    res.send('Api Obligatorio Desarrollo Web y Mobile 2023')
+});
+
+// Conexion a la base de datos //
+mongoose
+  .connect("mongodb://root:weberos@localhost:27017/test?authSource=admin&w=1")
+  .then(() => console.log('Conectado a MongoDB'))
+  .catch((error: any) => console.error(error));
 
 // Login //
-app.post('/api/login', (req, res) => {
+/*app.post('/api/login', (req, res) => {
     const user = req.body.user;
     const password = req.body.password;
     if (user === 'admin' && password === 'admin') {
@@ -45,7 +51,7 @@ function validateToken(req: any, res: any, next: any){
         res.state(401);
         res.send('No autorizado', 'anashe');
     }
-};
+};*/
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
